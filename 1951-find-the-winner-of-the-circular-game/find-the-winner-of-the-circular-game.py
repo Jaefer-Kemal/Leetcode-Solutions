3c3b06b46ef511c09d602429c5d7a7172d1c1fcc
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-10 21:07:33 +0300

    Time: 243 ms (5.35%) | Memory: 16.7 MB (13.90%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,24 +1,47 @@
+from collections import deque
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        count = 1
+        
         arr = [int(x) for x in range(1,n+1)]
-        out =  set()
+        d = deque(arr)
+        count = 1
+        
+        while len(d) != 1:
+            
+            if count % k == 0:
+                d.popleft()
+            else:
+                d.append(d[0])
+                d.popleft()
+            
+            count += 1
+        
+        return d[0]
+                
         
         
-        for i in range(n*100000000):
-            num = arr[i % n]
+        
+        
+        
+        
+        
+#         out =  set()
+#         count = 1
+        
+#         for i in range(n*100000000):
+#             num = arr[i % n]
             
-            if (len(out) == n-1) and (num not in out):
-                print(num)
-                return num
+#             if (len(out) == n-1) and (num not in out):
+#                 print(num)
+#                 return num
             
-            if count % k == 0 and len(out) != n-1:
-                if num not in out:
-                    out.add(num)
-                    count += 1
+#             if count % k == 0 and len(out) != n-1:
+#                 if num not in out:
+#                     out.add(num)
+#                     count += 1
             
-            if num not in out:
-                count += 1
+#             if num not in out:
+#                 count += 1
                 
                 
         
