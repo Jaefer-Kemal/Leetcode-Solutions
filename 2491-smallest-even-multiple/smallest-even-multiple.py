551ea994e953b2b96ad9c2377e02ea4fe1490b97
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-11 14:00:21 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (17.75%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.py
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -1,5 +1,10 @@
-from math import gcd
+
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        return (2 * n)//gcd(n,2)
+        def gcd(a,b):
+            if a == 0:
+                return b
+            return gcd(b % a, a)
+
+        return (2 * n //gcd(n,2))
         
