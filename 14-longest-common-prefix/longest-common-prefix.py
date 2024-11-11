e54a5a49f2964954474d29a581af0222e4b7b83b
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-11 22:01:53 +0300

    Time: 4 ms (11.70%) | Memory: 16.9 MB (13.77%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,11 +1,34 @@
-import os
+from collections import deque
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs.sort()
-        prefix=""
+        l = 0
+        for n in strs:
+            if n == "":
+                return ""
+
+        c = strs[0][0]
+        strs = sorted(strs)
+        c = list(strs[0])
+        flag = False
         for i in range(len(strs[0])):
-            if strs[0][i]==strs[-1][i]:
-                prefix+=strs[0][i]
-            else:
-                return prefix
-        return prefix
+
+            for j in range(len(strs)):
+                if c[i] != strs[j][i]:
+                    flag = True
+                    
+            if flag:
+                break
+            l += 1
+        if l == 0:
+            return ""
+        return "".join(c[:l])
+
+                
+
+
+
+        
+
+
+
+        
