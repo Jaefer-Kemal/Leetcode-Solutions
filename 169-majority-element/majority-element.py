95793c346b5de5c4f9ccf6dce659079c5f28e4dd
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-11 12:57:57 +0300

    Time: 7 ms (57.93%) | Memory: 18.5 MB (7.59%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -3,13 +3,11 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         c = Counter(nums)
         
-        maxim = -1
-        res = nums[0]
+
+        n = len(nums)
+        n = n//2
         for key, value in c.items():
-            if value > maxim:
-                maxim = value
-                res = key
-        
-        return res
+            if value > n:
+                return key
         
         
