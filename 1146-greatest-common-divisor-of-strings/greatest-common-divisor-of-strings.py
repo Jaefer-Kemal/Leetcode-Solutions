5e1636183e8b225ef8f8adf0301ec57d5253f3b7
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-11 20:44:28 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.15%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        def gcd(a,b):
+            if a == 0:
+                return b
+            return gcd(b%a,a)
+
+        len1 = len(str1)
+        len2 = len(str2)
+
+        gc = gcd(len1,len2)
+
+        div1 = len1//gc
+        div2 = len2//gc
+
+        sliced = str1[:gc]
+        print(sliced)
+        if div1 * sliced == str1 and div2 * sliced == str2:
+            return sliced
+        return ""
