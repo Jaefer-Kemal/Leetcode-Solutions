b612952b9e4b83dfe54028d78b035ebdc678afbd
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-11 12:48:25 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (11.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,33 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(nums) - 1
-        dict_num = {}
-        for i, v in enumerate(nums):
-            dict_num[i] = int(v)
-        
-        dict_num = sorted(dict_num.items(), key = lambda x:x[1])
-
-
-        while l < r:
-            sum_ =  dict_num[l][1] + dict_num[r][1]
-
-            if sum_ == target:
-                return( [dict_num[l][0],dict_num[r][0]] )
-            
-            elif sum_ > target:
-                r -= 1
-                
+        used = {}
+        for i in range(len(nums)):
+            if target - nums[i] in used:
+                return [i,used.get(target-nums[i])]
             else:
-                l += 1
+                used[nums[i]] = i
+#         l = 0
+#         r = len(nums) - 1
+#         dict_num = {}
+#         for i, v in enumerate(nums):
+#             dict_num[i] = int(v)
         
+#         dict_num = sorted(dict_num.items(), key = lambda x:x[1])
+
+
+#         while l < r:
+#             sum_ =  dict_num[l][1] + dict_num[r][1]
+
+#             if sum_ == target:
+#                 return( [dict_num[l][0],dict_num[r][0]] )
+            
+#             elif sum_ > target:
+#                 r -= 1
+                
+#             else:
+#                 l += 1
+
+                        
+                
+                        
+                
