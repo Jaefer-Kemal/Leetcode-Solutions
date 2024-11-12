8eef27b6332ae2032b1e89949dc3b063a6b90ca2
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-12 15:25:30 +0300

    Time: 8 ms (31.31%) | Memory: 17.7 MB (77.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,17 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = 0
-        r = 1
-        while r<len(nums):
-            if nums[l] == 0 and nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r = l + 1
-            elif nums[l] == 0 and nums[r] == 0:
-                r += 1
-            else:
-                l += 1
-                r = l + 1
+        read = 0
+        write = 0
+
+        while read < len(nums):
+            if nums[read] != 0:
+                nums[read], nums[write] = nums[write], nums[read]
+                
+                write += 1
+            read += 1
+
+        '''First Solution'''
+        # l = 0
+        # r = 1
+        # while r<len(nums):
+        #     if nums[l] == 0 and nums[r] != 0:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r = l + 1
+        #     elif nums[l] == 0 and nums[r] == 0:
+        #         r += 1
+        #     else:
+        #         l += 1
+        #         r = l + 1
             
             
