b8a5fb0c2d004a0c5b4ae419b63048af32f64c82
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-19 21:02:27 +0300

    Time: 43 ms (62.52%) | Memory: 27.3 MB (98.81%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -2,18 +2,37 @@ class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         skill.sort()
         check = set()
-        merge = []
         ans = 0
-        for i in range(0,len(skill)//2):
-            ch = skill[i] * skill[-(i + 1)]
+        l = 0
+        r = len(skill) - 1
+        while l < r:
+            ch = skill[l] * skill[r]
+            total = skill[l] + skill[r]
             
-            total = skill[i] + skill[-(i + 1)]
-            if i == 0:
+            if not check:
                 check.add(total)
             if total not in check:
                 return -1
+           
             ans += ch
-        
+            
+            l += 1
+            r -= 1
+            
         return ans
+            
+        
+'''First Solution'''
+#         for i in range(0,len(skill)//2):
+#             ch = skill[i] * skill[-(i + 1)]
+            
+#             total = skill[i] + skill[-(i + 1)]
+#             if i == 0:
+#                 check.add(total)
+#             if total not in check:
+#                 return -1
+#             ans += ch
+        
+#         return ans
            
         
