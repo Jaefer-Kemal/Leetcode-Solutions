9a6ffb6b5a282c39ddcd9526895b6ed13f01e602
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-24 20:51:41 +0300

    Time: 1 ms (13.99%) | Memory: 16.6 MB (64.47%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,18 +1,33 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        l, r = 0, 0
-        if len(haystack) < len(needle):
+        
+        if len(needle) > len(haystack):
             return -1
-        while l < len(haystack):
-            i = l
-            while l < len(haystack) and r < len(needle) and haystack[l] == needle[r]:
-                l += 1
-                r += 1
-            if r ==len(needle):
-                return i
-            
-            l = i + 1
-            
-            r = 0
+
+        for r in range(len(haystack)):
+            if haystack[r: r + len(needle)] == needle:
+                return r
+        
         return -1
+            
+
+
+
+
+        '''First Solution'''
+        # l, r = 0, 0
+        # if len(haystack) < len(needle):
+        #     return -1
+        # while l < len(haystack):
+        #     i = l
+        #     while l < len(haystack) and r < len(needle) and haystack[l] == needle[r]:
+        #         l += 1
+        #         r += 1
+        #     if r ==len(needle):
+        #         return i
+            
+        #     l = i + 1
+            
+        #     r = 0
+        # return -1
             
