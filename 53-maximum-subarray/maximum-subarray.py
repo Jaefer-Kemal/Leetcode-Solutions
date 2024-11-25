0af56d879cbad044f5365bc19494d8a8125e012f
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-11-26 00:21:15 +0300

    Time: 51 ms (73.31%) | Memory: 31.4 MB (35.17%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,13 +3,12 @@ class Solution:
         cur_sum = 0
         count = 0
         maximum = float("-inf")
+
         for r in range(len(nums)):
             cur_sum += nums[r]
-            
             maximum = max(maximum,cur_sum)
             if cur_sum < 0:
                 cur_sum = 0
                
-        
         return maximum
         
