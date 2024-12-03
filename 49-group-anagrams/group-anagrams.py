9d8edd6441ca31ff48caa83aeee96fcc4fcb78a9
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-03 21:16:23 +0300

    Time: 15 ms (48.84%) | Memory: 22.1 MB (9.36%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        cnt = {}
+        for s in strs:
+            ls = [0] * 26
+            for char in s:
+                ls[ord(char) - ord("a")] += 1
+            ls = tuple(ls)
+            if ls not in cnt:
+                cnt[ls] = [s]
+            else:
+                cnt[ls] += [s]
+            
+        return list(cnt.values())
+            
+            
+
+
+        
