f260d5065837affda26990b13c418ef85b2c141a
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-05 00:30:57 +0300

    Time: 4063 ms (15.76%) | Memory: 33.7 MB (22.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,21 +3,15 @@ class Solution:
         if nums == []:
             return 0
         s = set(nums)
-        cnt = 0
         longest = 1
-        seen = {}
+
         for r in range(len(nums)):
             if nums[r] - 1 not in s:
                 num = nums[r]
+                cnt = 0
                 while num in s:
-                    if num in seen:
-                        cnt += seen[num]
-                        break
                     cnt += 1
                     num += 1
-
-                seen[nums[r]] = cnt 
                 longest = max(longest, cnt)
-            cnt = 0
 
         return longest
