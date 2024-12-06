0ca500c8530567675e5fedbf85562254ce8f2835
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-06 22:43:38 +0300

    Time: 0 ms (100.00%) | Memory: 17.4 MB (6.42%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,28 +1,47 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        rules = {"..","."}
-        ls = deque(path.split("/"))
-        res = ["/"]
-        while ls:
-            print(ls)
-            if ls[0] == '':
-                ls.popleft()
-                continue
 
-            elif ls[0] not in rules:
-                res.append(f"{ls.popleft()}/")
+        ls = deque(path.split("/"))
+        res = []  # Use a simple list to store valid directories
+        
+        while ls:
+            curr = ls.popleft()
+            if not curr or curr == ".":  # Ignore empty or '.' segments
+                continue
+            elif curr == "..":  # Handle parent directory
+                if res:  # Pop only if res is not empty
+                    res.pop()
             else:
-                if ls[0] == ".":
-                    ls.popleft()
-                    continue
-                elif ls[0] == "..":
-                    if len(res) != 1:
-                        ls.popleft()
-                        res.pop()
-                    else:
-                        ls.popleft()
-        if len(res) == 1:
-            return "/"
-        return "".join(res)[:-1]
+                res.append(curr)  # Add valid directory
+        
+        # Join the result to form the simplified path
+        return "/" + "/".join(res)
+
+    '''First Solution'''
+    
+        # rules = {"..","."}
+        # ls = deque(path.split("/"))
+        # res = ["/"]
+        # while ls:
+        #     print(ls)
+        #     if ls[0] == '':
+        #         ls.popleft()
+        #         continue
+
+        #     elif ls[0] not in rules:
+        #         res.append(f"{ls.popleft()}/")
+        #     else:
+        #         if ls[0] == ".":
+        #             ls.popleft()
+        #             continue
+        #         elif ls[0] == "..":
+        #             if len(res) != 1:
+        #                 ls.popleft()
+        #                 res.pop()
+        #             else:
+        #                 ls.popleft()
+        # if len(res) == 1:
+        #     return "/"
+        # return "".join(res)[:-1]
                     
         
