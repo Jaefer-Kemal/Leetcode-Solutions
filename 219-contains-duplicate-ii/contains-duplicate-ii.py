f25a98b4f6d0c8aa1c6cb91a98b71ee2959e116d
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-09 21:08:27 +0300

    Time: 12 ms (97.52%) | Memory: 30.4 MB (16.26%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,26 +1,33 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = defaultdict(int)
-
-        for r in range(k + 1):
-            if len(nums) == r:
-                return False
-            seen[nums[r]] += 1
-            if seen[nums[r]] > 1:
-                return True
-            
-        l = 0
-        for r in range(k + 1,len(nums)):
-            seen[nums[l]] -= 1
-            if seen[nums[l]] == 0:
-                del seen[nums[l]]
-            seen[nums[r]] += 1
-            if seen[nums[r]] > 1:
-                return True
-            l += 1
+        seen = {}
+        for r in range(len(nums)):
+            if nums[r] in seen:
+                if r - seen[nums[r]] <= k:
+                    return True
+            seen[nums[r]] = r
         return False
 
 
+
+    '''First Solution'''
+        # seen = defaultdict(int)
+
+        # for r in range(k + 1):
+        #     if len(nums) == r:
+        #         return False
+        #     seen[nums[r]] += 1
+        #     if seen[nums[r]] > 1:
+        #         return True
             
-                
-        
+        # l = 0
+        # for r in range(k + 1,len(nums)):
+        #     seen[nums[l]] -= 1
+        #     if seen[nums[l]] == 0:
+        #         del seen[nums[l]]
+        #     seen[nums[r]] += 1
+        #     if seen[nums[r]] > 1:
+        #         return True
+        #     l += 1
+        # return False
+
