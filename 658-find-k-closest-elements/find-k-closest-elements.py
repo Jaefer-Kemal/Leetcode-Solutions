704f917dba1bd4558866ad1ed2610d33a4fee91d
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-09 20:57:33 +0300

    Time: 4 ms (65.53%) | Memory: 18.6 MB (28.18%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        res = deque()
+    
+        for num in arr:
+            if res:
+                if k == 1:
+                    if res[0] == num:
+                        continue
+                    elif abs(res[0] - x) > abs(num - x):
+                        res.popleft()
+                        res.append(num)
+                    else:
+                        break
+                else:
+                    res.append(num)
+                    k -= 1
+            else:
+                res.append(num)
+        return list(res)
+        
