f3e864151a0d2a33790d77674728b8cfd289de31
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-13 00:11:28 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (6.68%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        res = ""
+        res = []
         flag = True
         for r in range(0,len(s),k):
             if flag:
-                res += s[r:r+k][::-1]
-                flag = False
+                res.append(s[r:r+k][::-1])
             else:
-                res += s[r:r+k]
-                flag = True
-        return res
+                res.append(s[r:r+k])
+            flag = not flag 
+        return "".join(res)
 
         
