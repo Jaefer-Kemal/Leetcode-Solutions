be8d2b953231bbec393ff20bcd01155bf428d687
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2024-12-15 17:56:55 +0300

    Time: 0 ms (100.00%) | Memory: 18.3 MB (15.76%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,36 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        used = {}
-        l = 0
-        while l < len(nums):
-            if nums[l] in used:
-                return [l,used[nums[l]]]
-            else:
-                used[target - nums[l]] = l
-            l += 1
-'''First Solution'''
-#         l = 0
-#         r = len(nums) - 1
-#         dict_num = {}
-#         for i, v in enumerate(nums):
-#             dict_num[i] = int(v)
-        
-#         dict_num = sorted(dict_num.items(), key = lambda x:x[1])
-
-
-#         while l < r:
-#             sum_ =  dict_num[l][1] + dict_num[r][1]
-
-#             if sum_ == target:
-#                 return( [dict_num[l][0],dict_num[r][0]] )
-            
-#             elif sum_ > target:
-#                 r -= 1
-                
-#             else:
-#                 l += 1
-
-                        
-                
-                        
-                
+        seen = {}
+        n = len(nums)
+        for r in range(n):
+            if target - nums[r] in seen:
+                return [r,seen[target - nums[r]]]
+            if nums[r] not in seen:
+                seen[nums[r]] = r
