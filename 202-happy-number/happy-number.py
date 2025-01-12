a9ded2e3324eb17aeae796b7844472b5b58915ba
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-01-12 09:57:56 +0300

    Time: 3 ms (44.01%) | Memory: 17.6 MB (44.33%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,16 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if n < 10:
-            num = str(n * n)
-        else:
-            num = str(n)
-        flag = True
-        while len(num) > 1 or flag:
-            n = 0
-            if len(num) == 1:
-                flag = False
-            for i in num:
-                n += (int(i) * int(i))
-            num = str(n)
-            print(num)
-        return True if int(num) == 1 else False
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))  
+        return n == 1      
