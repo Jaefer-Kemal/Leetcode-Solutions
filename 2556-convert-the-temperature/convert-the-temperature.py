ff794ff3bb84b3a23fcfb13fe93e32098b707b76
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-01-30 10:03:40 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (42.18%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,7 +1,10 @@
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
         kelvin = celsius + 273.15
         fahrenheit = celsius * 1.80 + 32.00
-        return [kelvin,fahrenheit]
+        return [kelvin, fahrenheit]
         
