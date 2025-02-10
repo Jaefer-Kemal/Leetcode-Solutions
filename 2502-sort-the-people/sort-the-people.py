9d84a9e236a6d4dc9340fa1f5d7175c1be2ff46f
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-10 10:56:22 +0300

    Time: 387 ms (8.29%) | Memory: 18.3 MB (24.31%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,22 +1,38 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # Using Selection sort
+        '''Insertion Sort'''
         size = len(names)
-        for i in range(size):
-            min_index = i
-            biggest_element = heights[i]
-            for j in range(i + 1, size):
-                if biggest_element < heights[j]:
-                    min_index = j
-                    biggest_element = heights[j]
-            if i != min_index:
-                heights[i], heights[min_index] = heights[min_index], heights[i]
-                names[i], names[min_index] = names[min_index], names[i]
-            
+        for i in range(1, size):
+            j = i - 1
+            while j >= 0:
+                if heights[j] < heights[i]:
+                    heights[i], heights[j] = heights[j], heights[i]
+                    names[i], names[j] = names[j], names[i]
+                else:
+                    break
+                j -= 1
+                i -= 1
+        
         return names
+                
+
+        ''' Using Selection sort '''
+        # size = len(names)
+        # for i in range(size):
+        #     min_index = i
+        #     biggest_element = heights[i]
+        #     for j in range(i + 1, size):
+        #         if biggest_element < heights[j]:
+        #             min_index = j
+        #             biggest_element = heights[j]
+        #     if i != min_index:
+        #         heights[i], heights[min_index] = heights[min_index], heights[i]
+        #         names[i], names[min_index] = names[min_index], names[i]
+            
+        # return names
         
 
-        # Using Bubble sort
+        """ Using Bubble sort """
         # j = len(names)
         # while j >= 0:
         #     for i in range(0,j - 1):
