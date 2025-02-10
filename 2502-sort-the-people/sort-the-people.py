51025b4e586f8b53397b7acac3995ba468a1f7c5
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-10 10:35:54 +0300

    Time: 168 ms (12.39%) | Memory: 18 MB (88.03%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,12 +1,28 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        j = len(names)
-
-        while j >= 0:
-            for i in range(0,j - 1):
-                if heights[i] < heights[i + 1]:
-                    heights[i], heights[i + 1] = heights[i + 1], heights[i]
-                    names[i], names[i + 1] = names[i + 1], names[i]
-            j -= 1
-        
+        # Using Selection sort
+        size = len(names)
+        for i in range(size):
+            min_index = i
+            biggest_element = heights[i]
+            for j in range(i + 1, size):
+                if biggest_element < heights[j]:
+                    min_index = j
+                    biggest_element = heights[j]
+            if i != min_index:
+                heights[i], heights[min_index] = heights[min_index], heights[i]
+                names[i], names[min_index] = names[min_index], names[i]
+            
         return names
+        
+
+        # Using Bubble sort
+        # j = len(names)
+        # while j >= 0:
+        #     for i in range(0,j - 1):
+        #         if heights[i] < heights[i + 1]:
+        #             heights[i], heights[i + 1] = heights[i + 1], heights[i]
+        #             names[i], names[i + 1] = names[i + 1], names[i]
+        #     j -= 1
+        
+        # return names
