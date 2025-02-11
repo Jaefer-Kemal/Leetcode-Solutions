7dcc7312f58bc9efd6312817e04cdd84aba65b0f
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-11 11:12:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.87%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         str_nums = list(map(str,nums))
-        # multiplying the string by 10 will repeat it 10 time and then order lexicographically
-        res =  "".join(sorted(str_nums, key = lambda x: x * 10, reverse = True))
 
+        # Sort numbers based on their concatenated value
+        # We multiply each string by 10 to ensure consistent lexicographical comparison like "3" -> "3333333333" and "30" -> "30303030303030" 
+        # since its lexigraohical comparison it doesnt matter the length of the string.
+        str_nums.sort(key = lambda x: x * 10, reverse = True)
+
+        res = "".join(str_nums)
         if res[0] == "0":
             return "0"
         else:
