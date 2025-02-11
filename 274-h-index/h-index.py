0cf92c054f2121d24ee1431c81869c54a5263f81
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-11 15:21:31 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (37.17%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -3,23 +3,24 @@ class Solution:
         # First lets sort the citations:
         citations.sort()
 
-        no_of_paper = len(citations)
+        # Total number of paper 
+        total_papers = len(citations)
         
         maximum_h_index = 0
 
-        citations_dict = {}
+        # A dictionary to store remaining paper with at least k citations
+        # {citation : no_of_paper}
+        citations_paper_count = {}
 
-        for i in range(0, no_of_paper):
-            if citations[i] not in citations_dict and citations[i]:
-                number_of_citation = no_of_paper - i 
-                citations_dict[citations[i]] = number_of_citation
+        for i in range(0, total_papers):
+            if citations[i] not in citations_paper_count:
+                remaining_papers = total_papers - i 
+                citations_paper_count[citations[i]] = remaining_papers
         
-        for citation, paper in citations_dict.items():
+        for citation, paper in citations_paper_count.items():
             if citation <= paper:
                 maximum_h_index = max(citation, maximum_h_index)
             else:
                 maximum_h_index = max(paper, maximum_h_index)
                 
         return maximum_h_index
-            
-            
