9dffc334ad7392a089e895e5792e132551770237
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-11 09:50:37 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.75%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,24 +1,27 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        zero=0
-        one=0
-        two=0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                zero+=1
-            elif nums[i]==1:
-                one+=1
-            elif nums[i]==2:
-                two+=1
-                
-        for i in range(len(nums)):
-            if zero!=0:
-                nums[i]=0
-                zero-=1
-            elif one!=0:
-                nums[i]=1
-                one-=1
-            elif two!=0:
-                nums[i]=2
-                two-=1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeroes_count = 0
+        ones_count = 0
+        twos_count = 0
+        size = len(nums)
+        for i in range(size):
+            if nums[i] == 0:
+                zeroes_count += 1
+            elif nums[i] == 1:
+                ones_count += 1
+            else:
+                twos_count += 1
+        
+        for i in range(size):
+            if zeroes_count:
+                nums[i] = 0
+                zeroes_count -= 1
+            elif ones_count:
+                nums[i] = 1
+                ones_count -= 1
+            else:
+                nums[i] = 2
         
