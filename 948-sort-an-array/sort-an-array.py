edc0ef49af97f123e55617541a780182d1f9a9c1
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-11 10:31:05 +0300

    Time: 134 ms (80.56%) | Memory: 29.9 MB (18.06%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -3,22 +3,16 @@ class Solution:
         nums_count = defaultdict(int)
 
         maximum_element = max(nums) + 1
-        minimum_element = min(min(nums) , 0)
+        minimum_element = min(nums)
         
-        size = maximum_element + minimum_element
-
         for i in range(len(nums)):
             nums_count[nums[i]] += 1
 
         i = 0
         for num in range(minimum_element, maximum_element):
-            while num in nums_count:
+            while nums_count[num] > 0:
                 nums[i] = num
                 nums_count[num] -= 1
                 i += 1
-                if nums_count[num] == 0:
-                    del nums_count[num]
 
         return nums
-            
-        
