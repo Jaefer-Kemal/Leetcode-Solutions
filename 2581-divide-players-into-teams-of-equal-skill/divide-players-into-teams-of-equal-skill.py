cca29b871b42e205f99729b27fbd38464d5a4d9c
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-12 18:25:49 +0300

    Time: 37 ms (81.55%) | Memory: 28.8 MB (67.84%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -2,17 +2,20 @@ class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         skill.sort()
 
-        teams_chemistry = 0
+        total_teams_chemistry = 0
         left = 0
         right = len(skill) - 1
-        # Lets Assume that the first player and last player will be teamed up
+        # Lets Assume that the lowest skilled player will team up with highest skill player
         total_skill = skill[left] + skill[right]
+
         while left < right:
+
             if total_skill != skill[left] + skill[right]:
                 return -1
-            teams_chemistry += (skill[left] * skill[right])
+                
+            total_teams_chemistry += (skill[left] * skill[right])
             left += 1
             right -= 1
 
-        return teams_chemistry
+        return total_teams_chemistry
         
