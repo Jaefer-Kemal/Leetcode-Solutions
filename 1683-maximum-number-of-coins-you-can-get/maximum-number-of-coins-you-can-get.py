a8691ca30e7d251dcb47753ef8046d2f08928527
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-12 09:43:59 +0300

    Time: 83 ms (17.44%) | Memory: 25.1 MB (93.52%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -1,14 +1,18 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         my_piles = 0
-        piles = deque(sorted(piles))
-        while len(piles) >= 3:
+        piles.sort()
+        i = 0
+        j = len(piles)
+        while j - i >= 3:
             # Alice pile since she always pick the maximum number of coins
             piles.pop()
+            j -= 1
             # I will pick second maximum number of coins after Alice
             my_piles += piles.pop()
+            j -= 1
             # Bob will pick the smallest number of coins
-            piles.popleft()
+            i += 1
 
         return my_piles
 
