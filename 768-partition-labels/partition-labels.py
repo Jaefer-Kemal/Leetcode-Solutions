f188ffdcd0391b999200036a0f6de4198a3c3c85
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-13 15:47:06 +0300

    Time: 1 ms (95.50%) | Memory: 17.7 MB (54.09%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,20 +1,31 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        # We will count all the characters frequency
         chars_count = Counter(s)
-        results = []
-        fullfilled_set = set()
-        part_dict = defaultdict(int)
+        # The result that we are going to return
+        partition_sizes = []
+        # checking characters that will be included as part
+        current_partition = defaultdict(int)
+        # If we manage to collect every character in the current partition
+        # and their frequency match with the char:frequency in the chars_count
+        # we will append that specific character
+        completed_chars = set()
         left = 0
         for right in range(len(s)):
-            part_dict[s[right]] += 1
-            if part_dict[s[right]] == chars_count[s[right]]:
-                fullfilled_set.add(s[right])
-            
-            if fullfilled_set and len(fullfilled_set) == len(part_dict):
-                print(s[left:right])
-                results.append(right - left + 1)
-                left = right + 1
-                fullfilled_set = set()
-                part_dict = defaultdict(int)
 
-        return results
+            current_partition[s[right]] += 1
+            # if the frequncy of character matches then that mean
+            # we are sure that this specific character wont be appearing
+            # in another partitions
+            if current_partition[s[right]] == chars_count[s[right]]:
+                completed_chars.add(s[right])
+            # If we manage to have those character appearing only in this partition
+            # then we have fullfilled the condition for this part
+            # and we will create another partition and repeat the same process
+            if completed_chars and len(completed_chars) == len(current_partition):
+                partition_sizes.append(right - left + 1)
+                left = right + 1
+                completed_chars = set()
+                current_partition = defaultdict(int)
+
+        return partition_sizes
