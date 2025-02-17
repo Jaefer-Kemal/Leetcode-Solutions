a6d1843811532df1745bceb4c9702b4f1798fe34
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-17 15:00:23 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.50%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        accumulator = 0
-
-        for i in range(len(nums)):
-            accumulator += nums[i]
-            nums[i] = accumulator
-        return nums
+        running_sum = [nums[0]]
+        sum_ = nums[0]
+        for i in range(1, len(nums)):
+            sum_ += nums[i]
+            running_sum.append(sum_)
         
+        return running_sum
