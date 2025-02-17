c7c0896d21d6b178aa1e790758990bf3a6fadb70
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-17 18:06:38 +0300

    Time: 58 ms (68.79%) | Memory: 41.1 MB (96.56%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,43 +1,27 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        n = len(s)
-        prefix_sum = [0] * n
-        res = []
-        for shift in shifts:
-            direction = 1 if shift[2] == 1 else -1
-            l = shift[0]
-            r = shift[1] + 1
-            prefix_sum[l] += direction
-            if r != n:
-                prefix_sum[r] -= direction
-
-        # res.append(chr((ord(s[0]) + prefix_sum[0] - ord("a")) % 26 + ord("a")))
-        shift = 0
-        for r in range(n):
-            shift += prefix_sum[r]
-            res.append(chr((ord(s[r]) + shift - ord("a")) % 26 + ord("a")))
-
-        return "".join(res)
-
-
-
-
-
-        # res =  ""
-        # cnt = defaultdict(int)
-
-        # for shift in shifts:
-        #     direction = 1 if shift[2] == 1 else -1
-        #     if shift[0] == shift[1]:
-        #         cnt[shift[0]] += direction
-        #     else:
-        #         for i in range(shift[0],shift[1] + 1):
-        #             cnt[i] += direction
-                
+        n = len(s) 
+        prefix = [0] * (n + 2)
         
-        # for  idx, char in enumerate(s):
-        #     res += chr((ord(char) + cnt[idx] - ord("a")) % 26 + ord("a"))
+        for start, end, direction in shifts:
+            if direction:
+                steps = 1
+            else:
+                steps = -1
+
+            prefix[start] += steps
+            prefix[end + 1] -= steps
         
-        # return res
-                            
+        for i in range(1, n + 2):
+            prefix[i] += prefix[i - 1]
         
+        final_string = [0] * n
+        for i in range(n):
+            updated_char = (ord(s[i]) - ord("a"))  + prefix[i]
+            updated_char = chr((updated_char % 26) + ord("a"))
+
+            final_string[i] = updated_char
+        
+        return "".join(final_string)
+
+            
