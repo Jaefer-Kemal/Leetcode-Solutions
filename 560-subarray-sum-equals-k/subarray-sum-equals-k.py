1fc890fe1897daf973c6e5da5ba84055e590491b
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-18 14:41:14 +0300

    Time: 41 ms (25.41%) | Memory: 19.9 MB (99.91%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,23 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        cnt = {0:1}
         prefix_sum = 0
-        res = 0
-            
-        for r in range(len(nums)):
+        
+        cnt = defaultdict(int)
+        # Initialize it with 0 having frequqency of 1
+        cnt[0] = 1
+
+        result = 0
+        n = len(nums)
+
+        for r in range(n):
             prefix_sum += nums[r]
 
-            if prefix_sum - k in cnt:
-                res += cnt[prefix_sum - k]
-            
-            if prefix_sum not in cnt:
-                cnt[prefix_sum] = 1
-            else:
-                cnt[prefix_sum] += 1
-                
+            # If (prefix_sum - k) is in cnt, it means there's a subarray 
+            # ending at the current index that sums to k. Add its count to result.
 
-        return res
+            if (prefix_sum - k) in cnt:
+                result += cnt[prefix_sum - k] 
+                
+            cnt[prefix_sum] += 1
+
+        return result
