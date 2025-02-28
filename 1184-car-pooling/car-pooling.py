15329d4494c955460dabf2c9f71f5554d60b6da4
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-02-28 19:31:57 +0300

    Time: 4 ms (38.28%) | Memory: 18.3 MB (53.36%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,34 +1,17 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        stops = [0 for _ in range(1001)]
-        for t in trips:
-            stops[t[1]] += t[0]
-            stops[t[2]] -= t[0]
+        intervals = [0] * 1001
+
+        for trip in trips:
+            start = trip[1]
+            end = trip[2]
+            intervals[start] += trip[0]
+            intervals[end] -= trip[0]
         
-        for i in range(1001):
-            capacity -= stops[i]
-            if capacity < 0:
+
+        for i in range(1,len(intervals)):
+            if intervals[i - 1] > capacity:
                 return False
-        
-        return capacity >= 0
-        
-        
-#         dic = {}
-#         maxim = float("-inf")
-#         for i in range(len(trips)):
-            
-#             for j in range(trips[i][1],trips[i][2]):
-#                 if j not in dic:
-#                     dic [j] = trips[i][0]
-#                 else:
-#                     dic [j] += trips[i][0]
-#                 print(maxim)
-#                 if maxim < dic[j]:
-#                     maxim = dic[j]
-#                     if maxim > capacity:
-#                         return False
-                    
-#         if maxim > capacity:
-#             return False
-#         return True
-                    
+            intervals[i] += intervals[i - 1]
+
+        return True
