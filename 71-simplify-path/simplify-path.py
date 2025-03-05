6c9657b157c0229d9a2cc55c706efd7f41de658d
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-03-05 16:27:30 +0300

    Time: 1 ms (49.65%) | Memory: 17.7 MB (76.96%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,47 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        path = path.split("/")
 
-        ls = deque(path.split("/"))
-        res = []  # Use a simple list to store valid directories
-        
-        while ls:
-            curr = ls.popleft()
-            if not curr or curr == ".":  # Ignore empty or '.' segments
+        stack = []
+        for dire in path:
+            if (not dire) or dire == ".":
                 continue
-            elif curr == "..":  # Handle parent directory
-                if res:  # Pop only if res is not empty
-                    res.pop()
+            if dire == "..":
+                if stack:
+                    stack.pop()
             else:
-                res.append(curr)  # Add valid directory
+                stack.append(dire)
         
-        # Join the result to form the simplified path
-        return "/" + "/".join(res)
-
-    '''First Solution'''
-    
-        # rules = {"..","."}
-        # ls = deque(path.split("/"))
-        # res = ["/"]
-        # while ls:
-        #     print(ls)
-        #     if ls[0] == '':
-        #         ls.popleft()
-        #         continue
-
-        #     elif ls[0] not in rules:
-        #         res.append(f"{ls.popleft()}/")
-        #     else:
-        #         if ls[0] == ".":
-        #             ls.popleft()
-        #             continue
-        #         elif ls[0] == "..":
-        #             if len(res) != 1:
-        #                 ls.popleft()
-        #                 res.pop()
-        #             else:
-        #                 ls.popleft()
-        # if len(res) == 1:
-        #     return "/"
-        # return "".join(res)[:-1]
-                    
+        return "/" + "/".join(stack)
+            
         
