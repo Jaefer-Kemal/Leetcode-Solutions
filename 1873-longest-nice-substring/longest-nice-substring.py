d7cae065316b23c0d33c95b3501280a175353747
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-03-20 18:17:25 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (18.64%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,43 +1,44 @@
 class Solution:
     
     def longestNiceSubstring(self, s: str) -> str:
+        exist = set(s)
+        unwanted = []
 
-        def divide_and_conquer(s):
-            exist = set(s)
-            unwanted = []
-
-            for i, char in enumerate(s):
-                if not(char.lower() in exist and char.upper() in exist):
-                    unwanted.append(i)
-            
-            if len(unwanted) == 0:
-                return s
-            if len(unwanted) == len(s):
-                return ""
-            result = ""
-            for i, index in enumerate(unwanted):
-                if i==0:
-                    x = (divide_and_conquer(s[:index]))
-                    if result:
-                        if len(result) < len(x):
-                            result = x
-                    else:
-                        result = x
-                else:
-                    x = (divide_and_conquer(s[unwanted[i-1]+1: index]))
-                    if result:
-                        if len(result) < len(x):
-                            result = x
-                    else:
-                        result = x
-            x = (divide_and_conquer(s[unwanted[-1]+1:]))
-            if result:
-                if len(result) < len(x):
-                    result = x
-            else:
-                result = x
-            print(result)
-            return result
-    
-        return divide_and_conquer(s)
+        for i, char in enumerate(s):
+            if not(char.lower() in exist and char.upper() in exist):
+                unwanted.append(i)
         
+        if len(unwanted) == 0:
+            return s
+
+        if len(unwanted) == len(s):
+            return ""
+
+        result = ""
+        
+        for i, index in enumerate(unwanted):
+            if i==0:
+                word = (self.longestNiceSubstring(s[:index]))
+                if result:
+                    if len(result) < len(word):
+                        result = word
+                else:
+                    result = word
+            else:
+                word = (self.longestNiceSubstring(s[unwanted[i-1]+1: index]))
+
+                if result:
+                    if len(result) < len(word):
+                        result = word
+                else:
+                    result = word
+
+        word = (self.longestNiceSubstring(s[unwanted[-1]+1:]))
+
+        if result:
+            if len(result) < len(word):
+                result = word
+        else:
+            result = word
+
+        return result 
