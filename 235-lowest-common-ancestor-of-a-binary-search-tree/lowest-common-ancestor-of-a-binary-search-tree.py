9ed5e6fb336426731165456e9159483f8cfe3f93
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-03-23 16:09:09 +0300

    Time: 60 ms (48.70%) | Memory: 20.9 MB (81.71%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,39 +7,15 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val == p.val or root.val == q.val:
+            return root
 
-        def findPath(root, val):
-            if root is None:
-                return []
-            
-            if root.val == val:
-                return [root]
-            
-            if root.val > val:
-                left_path = findPath(root.left, val)
-                if left_path:
-                    return [root] + left_path
-            else:
-                right_path = findPath(root.right, val)
-                if right_path:
-                    return [root] + right_path
-            
-            return []
-
-        p_ancestor = findPath(root,p.val)
-        q_ancestor = findPath(root,q.val)
-
-        if len(p_ancestor) > len(q_ancestor):
-            path_set = set(p_ancestor)
-            smaller_path = q_ancestor
-            index = len(smaller_path) - 1
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right,p ,q)
         else:
-            path_set = set(q_ancestor)
-            smaller_path = p_ancestor
-            index = len(smaller_path) - 1 
-
-        while index >= 0:
-            if smaller_path[index] in path_set:
-                return smaller_path[index]
-            
-            index -= 1
+            # This happens when p and q are on different sides of root,
+            # so root is their lowest common ancestor.
+            return root
