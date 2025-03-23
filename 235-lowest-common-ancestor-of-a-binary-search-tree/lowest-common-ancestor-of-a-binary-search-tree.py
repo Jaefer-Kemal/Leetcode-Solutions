b0b3cb1c7c1c7fb331ea9c9cbcf90e94a3b76701
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-03-23 15:50:06 +0300

    Time: 68 ms (10.06%) | Memory: 21.3 MB (5.70%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,36 +8,38 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        def BST(root, val,res):
+        def findPath(root, val):
             if root is None:
                 return []
-
-            res.append(root)
-
-            if root.val == val:
-                return res
-
-            elif root.val > val:
-                res.extend(BST(root.left, val,res))
-            else:
-                res.extend(BST(root.right, val,res))
             
-            return res
+            if root.val == val:
+                return [root]
+            
+            if root.val > val:
+                left_path = findPath(root.left, val)
+                if left_path:
+                    return [root] + left_path
+            else:
+                right_path = findPath(root.right, val)
+                if right_path:
+                    return [root] + right_path
+            
+            return []
 
-        p_ancestor = BST(root,p.val,[])
-        q_ancestor = BST(root,q.val,[])
+        p_ancestor = findPath(root,p.val)
+        q_ancestor = findPath(root,q.val)
 
         if len(p_ancestor) > len(q_ancestor):
-            check = set(p_ancestor)
-            large = q_ancestor
-            n = len(large) - 1
+            path_set = set(p_ancestor)
+            smaller_path = q_ancestor
+            index = len(smaller_path) - 1
         else:
-            check = set(q_ancestor)
-            large = p_ancestor
-            n = len(large) - 1 
+            path_set = set(q_ancestor)
+            smaller_path = p_ancestor
+            index = len(smaller_path) - 1 
 
-        while n >= 0:
-            if large[n] in check:
-                return large[n]
+        while index >= 0:
+            if smaller_path[index] in path_set:
+                return smaller_path[index]
             
-            n -= 1
+            index -= 1
