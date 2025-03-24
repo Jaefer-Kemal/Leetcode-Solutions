a0ac23095c7c93d272bd9a48929f5cfd1d199a8c
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-03-24 17:44:01 +0300

    Time: 37 ms (58.83%) | Memory: 17.9 MB (5.16%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,21 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        def binarySearch(low, high, ans):
+            if low > high:
+                return ans
+            
+            mid = (low + high) // 2
+
+            if isBadVersion(mid):
+                ans = mid
+                return binarySearch(low ,mid - 1, ans)
+
+            else:
+                return binarySearch(mid + 1,high, ans)
+
+        
+        return binarySearch(0, n, n)
