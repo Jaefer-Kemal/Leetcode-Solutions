70a023c7935e0b472f41caec4ebaf276d45d3686
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-04-03 18:24:50 +0300

    Time: 0 ms (100.00%) | Memory: 18.4 MB (25.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,9 +2,9 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         low = 0
         high = len(matrix) - 1
-        
         row = None
-
+        
+        # First lets find the row that are in the range of the target
         while low <= high:
             mid = (low + high) // 2
 
@@ -18,14 +18,16 @@ class Solution:
             else:
                 low = mid + 1
 
+        # If theres no row thats possible for target to exist then its False
         if row == None:
             return False
 
         low = 0
         high = len(matrix[0]) - 1      
-
+        # And now we are going to perform binary search on the given row and see if the target exist
         while low <= high:
             mid = (low + high) // 2
+
             if matrix[row][mid] == target:
                 return True
 
