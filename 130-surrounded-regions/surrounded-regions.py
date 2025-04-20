35c2e86e74c8a71b6a93bba505e66d5d572018e8
Jaefer <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-04-20 20:35:01 +0300

    Time: 58 ms (9.48%) | Memory: 35.5 MB (5.03%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,40 @@
+class Solution:
+    def inbound(self, row, col, n, m):
+        return 0 <= row < n and 0 <= col < m
+
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = len(board)
+        m = len(board[0])
+        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        visited = [[False] * m for _ in range(n)]
+
+        def dfs(row, col, cells):
+            visited[row][col] = True
+            cells.append((row, col))
+            is_border = row == 0 or col == 0 or row == n - 1 or col == m - 1
+
+            for nr, nc in directions:
+                if self.inbound(row + nr, col + nc, n, m) and not visited[row + nr][col + nc]  and board[row + nr][col + nc] == "O":
+                    result = dfs(row + nr, col + nc, cells)
+                    is_border = is_border or result
+
+            return is_border
+        
+        all_changes = []
+        for row in range(n):
+            for col in range(m):
+                if not visited[row][col] and board[row][col] == "O":
+                    cells = []
+                    touch_board = dfs(row, col, cells)
+                    print(cells, touch_board)
+                    if not touch_board:
+                        all_changes.extend(cells)
+        
+        for r, c in all_changes:
+            board[r][c] = "X"
+
+
+        
