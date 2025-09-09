e6cb71bd423fa32d3946f4fce70f68490eb0c6c1
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-09 21:32:38 +0300

    Time: 7 ms (56.07%) | Memory: 19.4 MB (18.85%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count_freq = defaultdict(int)
-        maximum = float("-inf")
-        majority_element = None
+        max_count = len(nums) // 2
         for num in nums:
             count_freq[num] += 1
-            if maximum < count_freq[num]:
-                majority_element = num
-                maximum = count_freq[num]
+            if count_freq[num] > max_count:
+                return num
+            
         
         return majority_element
