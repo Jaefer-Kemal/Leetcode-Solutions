12788b3f6d0d61a500f3b16e7adc022ed1ba09b7
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-09 23:13:25 +0300

    Time: 11 ms (52.80%) | Memory: 20.3 MB (6.83%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.py
+++ 924-fair-candy-swap/fair-candy-swap.py
@@ -2,12 +2,11 @@ class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         total_alice = sum(aliceSizes)
         total_bob = sum(bobSizes)
-        mid = (total_alice - total_bob) // 2
+        diff = (total_alice - total_bob) // 2
 
         unique_alice = set(aliceSizes)
         unique_bob = set(bobSizes)
 
         for can in aliceSizes:
-            if can - mid in unique_bob:
-                return [can, can - mid]
-        
+            if can - diff in unique_bob:
+                return [can, can - diff]
