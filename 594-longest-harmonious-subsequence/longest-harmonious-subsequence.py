d6384ab4f8ed923a93963ecdbf7e361b0ec5754e
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-09 23:34:15 +0300

    Time: 42 ms (41.33%) | Memory: 19 MB (68.07%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -6,8 +6,5 @@ class Solution:
         for num in nums:
             if num - 1 in count_freq:
                 longest = max(longest, count_freq[num] + count_freq[num - 1])
-
-            if num + 1 in count_freq:
-                longest = max(longest, count_freq[num] + count_freq[num + 1])
         
         return longest
