fe71cd592ca41493fcc9662ddad587787795773b
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-09 22:48:37 +0300

    Time: 15 ms (9.46%) | Memory: 20.5 MB (87.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,16 +1,34 @@
-from collections import Counter
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        c = Counter(nums)
-        
-        n = len(nums)
-        n = n//3
+        candidates = defaultdict(int)
+
+        for num in nums:
+            if num in candidates:
+                candidates[num] += 1
+
+            elif len(candidates) < 2:
+                candidates[num] += 1
+            
+            else:
+                erase = []
+                for candidate in candidates.keys():
+                    candidates[candidate] -= 1
+                    if candidates[candidate] == 0:
+                        erase.append(candidate)
+                        
+                for e in erase:
+                    del candidates[e]
+
+
+        counts = defaultdict(int)
+        for num in nums:
+            if num in candidates:
+                counts[num] += 1
+
         res = []
-        for key, value in c.items():
-            if value > n:
-                res.append(key)
-                
-        return res
-                
+        for candidate, freq in counts.items():
+            if freq > len(nums) // 3:
+                res.append(candidate)
         
+        return res
+
