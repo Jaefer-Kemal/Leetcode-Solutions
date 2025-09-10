1961d2a3317f4939731d7f7dec4f9f8dae6268ad
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-10 22:50:46 +0300

    Time: 3 ms (73.89%) | Memory: 25.8 MB (20.59%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,27 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverse_list(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
         k %= len(nums)
         # reverse the whole list
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        reverse_list(0, len(nums) - 1)
         
         # reverse again starting fromm k -> end and from begin -> k
-        left = k
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-
-        left = 0
-        right = k - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        reverse_list(k, len(nums) - 1)
+        reverse_list(0, k - 1)
         
