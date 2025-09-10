4c7214c6489b79af30be0337ee85d7031f24fb14
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-10 18:17:42 +0300

    Time: 119 ms (34.31%) | Memory: 27 MB (32.54%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        buy = float("inf")
-        for r in range(len(prices)):
-            if buy > prices[r]:
-                buy = prices[r]
+        monotonic_stack = []
+        maximum_profit = 0
+        for price in prices:
             
-            sell = prices[r]
-            max_profit = max(sell - buy, max_profit)
-        
-        return max_profit
+            while monotonic_stack and monotonic_stack[-1] > price:
+                num = monotonic_stack.pop()
+                if monotonic_stack:
+                    maximum_profit = max(maximum_profit, num - monotonic_stack[0])        
 
+            monotonic_stack.append(price)
+        
+        if monotonic_stack and len(monotonic_stack) >= 2:
+            maximum_profit = max(maximum_profit, monotonic_stack[-1] - monotonic_stack[0])
+
+        return maximum_profit
+
+        
