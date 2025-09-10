7fca3741b0a419db8f34ff31ee3c01cbc54c53b2
Jaefer Mohammed Kemal <143391457+Jaefer-Kemal@users.noreply.github.com>
2025-09-11 01:16:49 +0300

    Time: 33 ms (59.05%) | Memory: 28.3 MB (96.10%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -1,33 +1,14 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        subarr_zero = []
         count = 0
         occurence = 0
-        memo = {}
         for num in nums:
             if num == 0:
                 count += 1
+                occurence += count
+            
             else:
-                subarr_zero.append(count)
                 count = 0
-        
-        subarr_zero.append(count)
-        print(subarr_zero)
-        for num in subarr_zero:
-            if num == 1:
-                occurence += 1
-                continue
-
-            if num in memo:
-                occurence += memo[num]
-                continue
-
-            if num != 0:
-                cnt = 0
-                for n in range(1,num + 1):
-                    cnt += n
-                    memo[n] = cnt
-
-                occurence += cnt
-
+            
         return occurence
+          
